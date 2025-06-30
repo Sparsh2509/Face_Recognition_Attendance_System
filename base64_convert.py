6b062f6deda8b594a1f6bd88f3e935587a7be39a
Sparsh2509 <sparshgupta250904@gmail.com>
2025-06-30 22:53:48 +0530

    new user added

--- base64_convert.py
+++ base64_convert.py
@@ -12,8 +12,24 @@
 # with open("sandeep_base64.txt", "w") as f:
 #     f.write(payload)
 
-# print("✅ Base64 written to sandeep_base64.txt")
+# print("Base64 written to sandeep_base64.txt")
 
+
+# import requests
+# import base64
+
+# url = "https://res.cloudinary.com/dzcwomu3h/image/upload/v1751193684/Achal_tkargi.jpg"
+# resp = requests.get(url)
+# resp.raise_for_status()
+
+# b64 = base64.b64encode(resp.content).decode("utf-8")
+# payload = "data:image/jpeg;base64," + b64
+
+# # Write to disk
+# with open("achal_base64.txt", "w") as f:
+#     f.write(payload)
+
+# print("Base64 written to achal_base64.txt")
 
 import requests
 import base64
@@ -26,7 +42,7 @@ b64 = base64.b64encode(resp.content).decode("utf-8")
 payload = "data:image/jpeg;base64," + b64
 
 # Write to disk
-with open("achal_base64.txt", "w") as f:
+with open("sparsh_base64.txt", "w") as f:
     f.write(payload)
 
-print("✅ Base64 written to achal_base64.txt")
+print("Base64 written to sparsh_base64.txt")
