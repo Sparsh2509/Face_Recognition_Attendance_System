b70fc9b2aae71ff8137f65626b9325fcbc24cd56
Sparsh2509 <sparshgupta250904@gmail.com>
2025-06-30 20:04:40 +0530

    new user test completed

--- base64_convert.py
+++ base64_convert.py
@@ -6,12 +6,19 @@
 # resp.raise_for_status()
 
 # b64 = base64.b64encode(resp.content).decode("utf-8")
-# print("data:image/jpeg;base64," + b64)
+# payload = "data:image/jpeg;base64," + b64
+
+# # Write to disk
+# with open("sandeep_base64.txt", "w") as f:
+#     f.write(payload)
+
+# print("✅ Base64 written to sandeep_base64.txt")
+
 
 import requests
 import base64
 
-url = "https://res.cloudinary.com/dzcwomu3h/image/upload/v1749470318/Sandeep_Test_zbehkm.jpg"
+url = "https://res.cloudinary.com/dzcwomu3h/image/upload/v1751193684/Achal_tkargi.jpg"
 resp = requests.get(url)
 resp.raise_for_status()
 
@@ -19,7 +26,7 @@ b64 = base64.b64encode(resp.content).decode("utf-8")
 payload = "data:image/jpeg;base64," + b64
 
 # Write to disk
-with open("sandeep_base64.txt", "w") as f:
+with open("achal_base64.txt", "w") as f:
     f.write(payload)
 
-print("✅ Base64 written to sandeep_base64.txt")
+print("✅ Base64 written to achal_base64.txt")
