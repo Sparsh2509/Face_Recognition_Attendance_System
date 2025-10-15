3b7a51896e21be16210a51689feb3627bf3948e5
Sparsh2509 <sparshgupta250904@gmail.com>
2025-10-15 21:27:46 +0530

    Code formatted

--- auto_finalize_attendance.py
+++ auto_finalize_attendance.py
@@ -28,30 +28,7 @@ async def auto_finalize_attendance():
                 log.out_status = "present"
                 print(f"[AUTO-FINALIZED] {log.name} marked OUT at 11:59 PM")
 
-            # # Mark ABSENT for users who never marked IN
-            # all_users_result = await session.execute(select(UserFace))
-            # all_users = all_users_result.scalars().all()
-            # all_user_ids = {user.user_id: user.name for user in all_users}
 
-            # present_result = await session.execute(
-            #     select(AttendanceLog.user_id).where(AttendanceLog.date == yesterday)
-            # )
-            # marked_user_ids = {row[0] for row in present_result.fetchall()}
-
-            # missing_users = [uid for uid in all_user_ids if uid not in marked_user_ids]
-            # print(f"[INFO] Found {len(missing_users)} users who did NOT mark IN on {yesterday}")
-
-            # for uid in missing_users:
-            #     session.add(AttendanceLog(
-            #         user_id=uid,
-            #         name=all_user_ids[uid],
-            #         date=yesterday,
-            #         in_time=None,
-            #         out_time=None,
-            #         in_status="absent",
-            #         out_status="absent"
-            #     ))
-            #     print(f"[MARKED ABSENT] {all_user_ids[uid]} marked ABSENT for {yesterday}")
 
             await session.commit()
             print("[SUCCESS] Auto-finalization completed.")
