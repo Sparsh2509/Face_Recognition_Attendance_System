632ae803005608d8c21bb7e6e51a590c574cb92d
Sparsh2509 <sparshgupta250904@gmail.com>
2025-10-21 16:04:34 +0530

    Readme work started

--- base64_convert.py
+++ base64_convert.py
@@ -1,36 +1,3 @@
-# import requests
-# import base64
-
-# url = "https://res.cloudinary.com/dzcwomu3h/image/upload/v1749470318/Sandeep_Test_zbehkm.jpg"
-# resp = requests.get(url)
-# resp.raise_for_status()
-
-# b64 = base64.b64encode(resp.content).decode("utf-8")
-# payload = "data:image/jpeg;base64," + b64
-
-# # Write to disk
-# with open("sandeep_base64.txt", "w") as f:
-#     f.write(payload)
-
-# print("Base64 written to sandeep_base64.txt")
-
-
-# import requests
-# import base64
-
-# url = "https://res.cloudinary.com/dzcwomu3h/image/upload/v1751193684/Achal_tkargi.jpg"
-# resp = requests.get(url)
-# resp.raise_for_status()
-
-# b64 = base64.b64encode(resp.content).decode("utf-8")
-# payload = "data:image/jpeg;base64," + b64
-
-# # Write to disk
-# with open("achal_base64.txt", "w") as f:
-#     f.write(payload)
-
-# print("Base64 written to achal_base64.txt")
-
 import requests
 import base64
 
