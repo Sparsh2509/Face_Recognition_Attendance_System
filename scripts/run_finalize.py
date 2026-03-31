8c6c75604608626a00070991c4cbd704fca1ac46
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-31 18:54:42 +0530

    Using github action again

--- scripts/run_finalize.py
+++ scripts/run_finalize.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import asyncio
 from auto_finalize_attendance import auto_finalize_attendance
 
