26a8feebd5e2e8c3ba7354187c6ee8f85c9801d4
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-31 13:25:39 +0530

    Project working successfully

--- scripts/base64_convert.py
+++ scripts/base64_convert.py
@@ -1,7 +1,7 @@
 import requests
 import base64
 
-url = "https://res.cloudinary.com/dzcwomu3h/image/upload/v1748963648/New_Image_ieiuem.jpg"
+url = "https://res.cloudinary.com/dzcwomu3h/image/upload/v1749470300/Achal_Test_g2gzox.jpg"
 resp = requests.get(url)
 resp.raise_for_status()
 
@@ -9,7 +9,7 @@ b64 = base64.b64encode(resp.content).decode("utf-8")
 payload = "data:image/jpeg;base64," + b64
 
 # Write to disk
-with open("sparsh_base64.txt", "w") as f:
+with open("achal_base64.txt", "w") as f:
     f.write(payload)
 
-print("Base64 written to sparsh_base64.txt")
+print("Base64 written to achal_base64.txt")
